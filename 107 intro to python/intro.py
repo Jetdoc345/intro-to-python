20bb1ae2c04378a7aec16f9679fee813c96b6ac2
Jetdoc345 <jw600345@gmail.com>
2024-11-13 14:16:57 -0500

    first commit first assignment

---
+++ 107 intro to python/intro.py
@@ -0,0 +1,41 @@
+name = "james" 
+last_name = "wade"
+cohort = 52
+is_active = True
+
+print(name) + " " + last_name + "#" + str(cohort)
+
+integer = 10  # Integer
+float_num = 3.14 # Float
+text = "Hello"  # String
+is_sunny = False # Boolean
+
+# ===== Type conversion ======
+num = 9.75
+print(int(num))   # convert a float to an integer
+
+age = 25
+print(str(age))   #convert an integer to a string
+
+price = "19.99"
+print(float(price))  # convert a string to a float, output:
+
+# challenge
+#Create a variable called: name, last_name, age, and show them in a print
+# "Hello, <name> <last_name>, you are <age> years old, "name = james"
+name = "james"
+last_name = "wade"
+age = 36
+print(f"Hello, {name} {last_name}, you are {str{age}} years old.")
+
+# ======Operators =========
+x= 5
+y= 3
+
+print {x+y} # addition
+print {x-y} # subtraction
+print {x*y} # multiplication
+print {x/y} # division
+print {x%y} # modulus
+print {x**y} # exponent
+
